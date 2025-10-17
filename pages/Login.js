7fbdd2d187fdbdf9b5fd28c93cc5223c56c4c9ae
Paresh Madav <madavparesh08@gmail.com>
2025-10-17 18:01:57 +0530

    Login page set

---
+++ pages/Login.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text,StyleSheet } from 'react-native';
+
+const Login = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Login Page</Text>
+    </View>
+  );
+}
+export default Login;
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+
+
+
